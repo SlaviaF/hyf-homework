485728615c52bae47ee3d0d610a82e58e2c5918d
SlaviaF <slaviafurtado@gmail.com>
2021-02-03 11:08:19 +0100

    added the total percentage of years spend on series

--- javascript/javascript1/week3/series.js
+++ javascript/javascript1/week3/series.js
@@ -33,7 +33,11 @@ function timeSpendOnSeries(seriesArr) {
     let totalDays = 0;
     let hoursInDays = 0;
     let minutesInDays = 0;
-    let total = "";
+    let total = 0;
+    let calculateTotalInDays = 0;
+    let daysInYears = 0;
+    let totalIndays = 0;
+    let totalPercent = 0;
 
 
 
@@ -48,16 +52,16 @@ function timeSpendOnSeries(seriesArr) {
         calculateTotalInDays = totalDays + hoursInDays + minutesInDays;
         daysInYears = calculateTotalInDays * 0.00273790926;
         percentage = (100 * daysInYears) / 80;
-        percentage = percentage.toFixed(3);
+        percentageUpto3decimals = percentage.toFixed(3);
         percentage;
+        console.log(seriesArr[i].title + ' took ' + percentageUpto3decimals + ' percentage of my life ')
 
 
-        console.log(seriesArr[i].title + ' took ' + percentage + ' of my life ')
         totalInDays = totalInDays + (seriesArr[i].days) + (seriesArr[i].hours * 0.0416666667) + (seriesArr[i].minutes * 0.000694444444);
-
+        total = percentage + total;
+        totalPercent = total.toFixed(3);
     }
-
-
+    console.log('In all it took ' + totalPercent + ' percentage of my life');
 }
 
 timeSpendOnSeries(seriesDurations);
