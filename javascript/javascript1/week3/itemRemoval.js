616061936894950e4a030ec766f0883d16e4edde
SlaviaF <slaviafurtado@gmail.com>
2021-02-03 09:08:28 +0100

    item Removal added

---
+++ javascript/javascript1/week3/itemRemoval.js
@@ -0,0 +1,23 @@
+//Item Array Removal
+
+
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+];
+const nameToRemove = names.splice(1, 1)
+console.log(nameToRemove);
+console.log(names);
+
+
+
+
+
+
+
