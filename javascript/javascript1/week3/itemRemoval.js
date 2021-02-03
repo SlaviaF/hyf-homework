23fe404be1ef9c1e497717e5c93debd517e9bc21
SlaviaF <slaviafurtado@gmail.com>
2021-02-03 16:02:08 +0100

    function to return Ahmad, but not as array

--- javascript/javascript1/week3/itemRemoval.js
+++ javascript/javascript1/week3/itemRemoval.js
@@ -11,13 +11,18 @@ const names = [
   "katrine",
   "Tala",
 ];
-const nameToRemove = names.splice(1, 1)
+/*const nameToRemove = names.splice(1, 1);
+
 console.log(nameToRemove);
+console.log(names);*/
+
+function removeItem(item, index) {
+  index = names.indexOf(item);
+  names.splice(index, 1);
+  return nameToRemove = item;
+
+}
+removeItem('Ahmad');
 console.log(names);
-
-
-
-
-
-
+console.log(nameToRemove);
 
