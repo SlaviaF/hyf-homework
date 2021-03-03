cd540a415c57f9459f321bac61695a9a74561c48
SlaviaF <slaviafurtado@gmail.com>
2021-03-03 22:56:23 +0100

    game Js file

---
+++ javascript/javascript2/week3/game.js
@@ -0,0 +1,91 @@
+const gameTime = document.getElementById("secondsForGame");
+const btn = document.getElementById("startButton");
+const gameOver = document.getElementById("gameOver");
+const win = document.getElementById("winner");
+const playerOnLead = document.getElementById("playerOnLead")
+
+let clickCountS;
+let clickCountL;
+let keySelect;
+
+btn.addEventListener('click', startGame)
+
+function startGame() {
+    window.addEventListener('keydown', playerfunc)
+    clickCountS = document.getElementById("clickCountS");
+    clickCountL = document.getElementById("clickCountL");
+    keySelect = document.getElementById("keySelection");
+
+    let keyS = 0;
+    let keyL = 0;
+    function playerfunc(e) {
+
+        if (e.keyCode == '83') {
+            clickCountS.innerText = keyS++;
+
+        } else if (e.keyCode == '76') {
+            clickCountL.innerText = keyL++;
+        }
+        else {
+            keySelect.innerText = `Select between 'S' or 'L' keys`
+            // alert(`Select between 'S' or 'L' keys`)
+        }
+
+        //extra feature
+        if (keyS > keyL) {
+            playerOnLead.innerHTML = `Player S on lead`
+        }
+        if (keyS < keyL) {
+            playerOnLead.innerHTML = `Player L on lead`
+        }
+        //
+        if (winnerObj.sPlayer) {
+            clickCountS.innerText = winnerObj.sPlayer;
+        }
+        if (winnerObj.lPlayer) {
+            clickCountL.innerText = winnerObj.lPlayer;
+        }
+        if (winnerObj.sPlayer > winnerObj.lPlayer) {
+            win.innerHTML = "Congrats Player S";
+
+        }
+        if (winnerObj.sPlayer < winnerObj.lPlayer) {
+            win.innerHTML = "Congrats Player L";
+        }
+        if (winnerObj.sPlayer == winnerObj.lPlayer) {
+            win.innerHTML = "Congrats Player L";
+        }
+    }
+}
+/*UNABLE TO ADD THE TIMER BEFORE THE END OF THE GAME
+btn.addEventListener('click', countDown);
+let countDownTime;
+function countDown() {
+    countDownTime = 3;
+    const timeLeftDisplay = document.getElementById("timeLeft");
+    setInterval(() => {
+        if (countDownTime <= 0) {
+            clearInterval(countDownTime = 0)
+        }
+        timeLeftDisplay.innerHTML = countDownTime;
+        countDownTime -= 1
+    }, 1000)
+
+}*/
+
+btn.addEventListener('click', gameComplete)
+winnerObj = { 'sPlayer': 0, 'lPlayer': 0 }
+function gameComplete() {
+    if (gameTime.value) {
+        setTimeout(() => {
+            gameOver.style.display = "block";
+            winnerObj.sPlayer = clickCountS.innerText;
+            winnerObj.lPlayer = clickCountL.innerText;
+            console.log(winnerObj);
+            clickCountS.innerText = winnerObj.sPlayer;
+            console.log(winnerObj.sPlayer);
+            clickCountL.innerText = winnerObj.lPlayer;
+        }, gameTime.value * 1000)
+
+    }
+}
