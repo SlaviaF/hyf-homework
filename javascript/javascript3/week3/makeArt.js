b2195269af539029d977a2fd84286f2465f01fc8
SlaviaF <slaviafurtado@gmail.com>
2021-03-25 21:13:15 +0100

    making ART circles

---
+++ javascript/javascript3/week3/makeArt.js
@@ -0,0 +1,36 @@
+const canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const c = canvas.getContext('2d')
+let circles = [];
+let numberOfCircles = 100;
+class Circles {
+    constructor(x, y, r, startAngle, endAngle, fillColor) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+        this.fillColor = fillColor;
+    }
+    drawCircle() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.r, this.startAngle, this.endAngle)
+        c.strokeStyle = this.fillColor;
+        c.fillStyle = c.strokeStyle;
+        c.stroke();
+        c.fill();
+    }
+}
+function generateCircles() {
+
+    const circle1 = new Circles(Math.random() * window.innerWidth,
+        Math.random() * window.innerHeight,
+        Math.floor(Math.random() * 50),
+        0,
+        Math.PI * 2,
+        ("#" + Math.floor(Math.random() * 16777215).toString(16)));
+    circle1.drawCircle()
+}
+
+setInterval(generateCircles, 100);
